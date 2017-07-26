8e87948f56dbf6613de8911eb97679494d94f721
stefan safranek <stefan@stefan.stefan>
2017-07-25 19:38:36 -0700

    added gitignore

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import requests
 
-resp = requests.post('http://localhost:8001/api/v1/queue/test', data={"test":True})
+resp = requests.post('http://localhost:4444/api/v1/queue/test', data={"test":True})
 print(resp.text)
 
-resp = requests.get('http://localhost:8001/api/v1/queue/test')
+resp = requests.get('http://localhost:4444/api/v1/queue/test')
 print(resp.text)
 
-resp = requests.post('http://localhost:8001/api/v1/queue/test', json={"test":True})
+resp = requests.post('http://localhost:4444/api/v1/queue/test', json={"test":True})
 print(resp.text)
